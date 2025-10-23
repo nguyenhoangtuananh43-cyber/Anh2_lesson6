a7affad93000fea5d73f658a6362119239fb5eeb
nguyenhoangtuananh43-cyber <nguyenhoangtuananh43@gmail.com>
2025-10-23 14:51:06 +0700

    Add files via upload

---
+++ AbsoluteNumberCalculatorTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbsoluteNumberCalculatorTest {
+
+    @Test
+    void findAbsolute() {
+        int number =0;
+        int expected = 0;
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+    @Test
+    void testFindAbsolute() {
+        int number = 1;
+        int expected = 1;
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+    @Test
+    void testFindNextDay() {
+        int number = -1;
+        int expected = -1;
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+
+}
